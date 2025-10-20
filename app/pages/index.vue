96b3eab61cb8c3f18e92cf46e52cee11044692c3
Steffen Kaschke <steffen.kaschke@googlemail.com>
2025-10-20 12:33:38 +0200

    Add first page and component

---
+++ app/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <h1>Title</h1>
+    <AppButton>Button</AppButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Resume',
+  meta: [
+    { name: 'description', content: 'My amazing site.' },
+  ],
+  bodyAttrs: {
+    class: 'test',
+  },
+  script: [{ innerHTML: 'console.log(\'Hello world\')' }],
+}),
+useSeoMeta({
+  title: 'Mein CV',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+<style>
+
+</style> 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'Nuxt', // default fallback title
+      htmlAttrs: {
+        lang: 'de',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api',
+    },
+  },
 })
